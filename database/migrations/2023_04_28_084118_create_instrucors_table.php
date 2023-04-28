9b714fc3b56699c463defdd11d6106fac4ecbe27
suzuki-ami-224 <amigo22409@gmail.com>
2023-04-29 01:20:29 +0900

    ログイン完了

--- database/migrations/2023_04_28_084118_create_instrucors_table.php
+++ database/migrations/2023_04_28_084118_create_instrucors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateInstrucorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('instructors', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('remember_token')->nullable();
-            $table->tinyInteger('role')->default(0);
+            $table->integer('jenre_id');
+            $table->string('picture');
+            $table->string('comment');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('instructors');
     }
 }
